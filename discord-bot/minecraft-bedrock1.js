37af7d0f5cf7f9938675d1ceb81c683385e1bbf9
u_i ( su_i ) <97729185+siso5@users.noreply.github.com>
2023-04-22 20:34:55 +0900

    Create minecraft-bedrock1.js

---
+++ discord-bot/minecraft-bedrock1.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const { Client, Intents } = require('discord.js');
+const Gamedig = require('gamedig');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  updateServerStatus();
+});
+
+function updateServerStatus() {
+  Gamedig.query({
+    type: 'minecraft',
+    host: 'ww[.]xxx[.]net',
+    port: 19132
+  }).then((state) => {
+    console.log(`Minecraft Bedrock is UP `);
+    client.user.setPresence({
+      activities: [{ name: `Minecraft Bedrock available` }],
+      status: 'online'
+    });
+  }).catch((error) => {
+    console.log('Minecraft Bedrock is DOWN', error);
+    client.user.setPresence({
+      activities: [{ name: 'Minecraft Bedrock unavailable' }],
+      status: 'dnd'
+    });
+  }).finally(() => {
+    setTimeout(updateServerStatus, 10 * 1000);
+  });
+}
+
+
+// ボットをログインさせる
+client.login('BOT_TOKEN');
